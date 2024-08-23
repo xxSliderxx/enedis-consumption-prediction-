6ee24bdbe4046b1a5d7e6ae37328aee4bb7dbdb3
Borowski Alexandre <114334792+xxSliderxx@users.noreply.github.com>
2024-08-23 19:50:02 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,23 @@
 import streamlit as st
 from eda import intro, ML,ML_explain
+import pandas as pd
+import numpy as np
+import folium
+from streamlit_folium import folium_static
+import plotly.express as px
+import plotly.graph_objects as go
+from matplotlib import pyplot as plt
+import seaborn as sns
+import re
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn import metrics
+from sklearn.decomposition import PCA
+from io import BytesIO
+import base64
+
 
 # Background
 page_bg_img = f"""
